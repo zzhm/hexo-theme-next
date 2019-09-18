a79aa446ed8d2b9a44d82e28bbae5320a2eb7b36
1v9 <mkb@live.cn>
2019-09-18 20:55:47 +0800

    regex

--- scripts/helpers/index.js
+++ scripts/helpers/index.js
@@ -15,12 +15,10 @@ hexo.extend.helper.register('next_env', type => {
 hexo.extend.helper.register('canonical', function() {
   const permalink = hexo.config.permalink;
   const canonical = hexo.theme.config.canonical;
-  if (!canonical) {
-    return '';
-  }
-  var url = this.url.replace('index.html', '');
+  if (!canonical) return '';
+  var url = this.url.replace(/index\.html$/, '');
   if (!permalink.endsWith('.html')) {
-    url = url.replace('.html', '');
+    url = url.replace(/\.html$/, '');
   }
   return `<link rel="canonical" href="${url}">`;
 });
